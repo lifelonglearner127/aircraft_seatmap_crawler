44f2fe9df4ab34fd018a5fc1b4e7ea999823521a
Zhen Yu <iamyourfriendforever127@protonmail.com>
2021-05-28 06:31:54 +0400

    update validated script

--- seatmap/validate_csv.py
+++ seatmap/validate_csv.py
@@ -3,4 +3,20 @@ import pandas as pd
 df = pd.read_csv("seatmaps.csv", delimiter=";")
 df2 = df.sort_values(by=["airline_name", "aircraft_code"], key=lambda col: col.str.lower())
 
-df2.to_csv("cleaned.csv", sep=";", index=False)
+df2.to_csv(
+    "cleaned.csv",
+    sep=";",
+    index=False,
+    columns=[
+        "airline_code",
+        "aircraft_code",
+        "aircraft_description",
+        "airline_name",
+        "layout",
+        "seat_map",
+        "traveler_photos",
+        "seat_map_key",
+        "overview",
+        "seats_file",
+    ],
+)
